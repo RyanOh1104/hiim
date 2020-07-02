32e204ac1c17aa697e68a7324af93510e1e2c181
Ryan <yyjo1104@naver.com>
2020-07-02 22:41:42 +0900

    logout btn

--- mvp/settings.py
+++ mvp/settings.py
@@ -100,6 +100,16 @@ DATABASES = {
         'PORT' : '',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME' : 'hiimmvp',  이거 뭐더라?
+#         'USER' : 'hiimmvp',
+#         'PASSWORD' : 'shris9494',
+#         'HOST' : PG_HOST,
+#         'PORT' : '',
+#     }
+}
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
