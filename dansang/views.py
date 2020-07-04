6c5773ca935b05e8b3391a2861d5de568538c0ee
Ryan <yyjo1104@naver.com>
2020-07-04 19:48:56 +0900

    minor change

--- dansang/views.py
+++ dansang/views.py
@@ -74,6 +74,20 @@ def dansangUpdate(request, authuser_id, slug):
         if form.is_valid():
             instance = form.save(commit=False)
             instance.authuser = request.user
+
+            # main에서 제목 display
+            if len(instance.title) >= 17:
+                instance.title = instance.title[0:17] + "..."
+            # main에서 부제목 display -- subtitle OR first_sentence
+            if len(instance.subtitle) >= 35:
+                instance.subtitle = instance.subtitle[0:35] + "..."
+            # Subtitle이 없다면 First Sentence로 대체
+            # PROBLEM : 첫문장이 짧고, 그 다음에 줄바꿈을 하면 그 줄바꿈(tag)이 그대로 subtitle에 반영된다.
+            # 즉, subtitle이 없는 경우 contents의 첫 부분을 가져오는데 문제는 html tag가 모두 포함된다는 것.
+            if instance.subtitle == "":
+                instance.first_sentence = instance.contents[0:35] + "..."
+                instance.subtitle = instance.first_sentence
+
             instance.slug = slugify(datetime.now())
             instance.url = "/dansang/dansangdetail/" + str(authuser_id) + '/' + str(instance.slug)
             thisDansang.delete()
