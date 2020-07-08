e45901f905ca4dd3ffe0fb2bc1089dfff4be251d
Ryan <yyjo1104@naver.com>
2020-07-08 12:39:49 +0900

    minor change

--- dansang/views.py
+++ dansang/views.py
@@ -85,7 +85,7 @@ def dansangUpdate(request, authuser_id, slug):
             # PROBLEM : 첫문장이 짧고, 그 다음에 줄바꿈을 하면 그 줄바꿈(tag)이 그대로 subtitle에 반영된다.
             # 즉, subtitle이 없는 경우 contents의 첫 부분을 가져오는데 문제는 html tag가 모두 포함된다는 것.
             if instance.subtitle == "":
-                instance.subtitle = " "
+                instance.subtitle = "&nbsp;"
 
             instance.slug = slugify(datetime.now())
             instance.url = "/dansang/dansangdetail/" + str(authuser_id) + '/' + str(instance.slug)
