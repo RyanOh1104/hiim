82bf47c7ffae0dc6c8af4b746e06a115330f51f6
Ryan <yyjo1104@naver.com>
2020-07-11 06:32:00 +0900

    minor change

--- dansang/admin.py
+++ dansang/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import DansangInput, DansangSeed
 
+class DansangAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+
 # Register your models here.
 admin.site.register(DansangInput)
 admin.site.register(DansangSeed)
+admin.site.register(DansangAdmin)
