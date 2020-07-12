8e60b032d824918a550b4280da0ba8a08c5c398d
Ryan <yyjo1104@naver.com>
2020-07-12 19:04:09 +0900

    test

--- mvp/settings.py
+++ mvp/settings.py
@@ -218,6 +218,7 @@ BOWER_INSTALLED_APPS = (
 CRISPY_TEMPLATE_PACK="bootstrap4"
 
 LOGIN_REDIRECT_URL = "/"
+LOGIN_REDIRECT_URL = "/inputuserinfo"
 LOGOUT_REDIRECT_URL = "/login"
 
 # Media 추가할 때 이거 해줘야됨. Telusko 튜토리얼 2시간 18분부터 봐라
