32614d6c362d440c1ad773060c3e1c0d87479846
Ryan <yyjo1104@naver.com>
2020-07-13 11:39:32 +0900

    minor change

--- mvp/settings.py
+++ mvp/settings.py
@@ -201,7 +201,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'everyday/static'),
     os.path.join(BASE_DIR, 'history/static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles')
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
