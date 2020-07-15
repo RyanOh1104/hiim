44fd3961957db75eee6fc3d33e053f889ca04992
Ryan <yyjo1104@naver.com>
2020-07-15 16:24:03 +0900

    everyday img

--- everyday/views.py
+++ everyday/views.py
@@ -19,7 +19,7 @@ def everydayinput(request):
 
     if request.method == 'POST':
         # form = EverydayInputForm(request.POST)
-        form = EverydayInputForm(request.POST)
+        form = EverydayInputForm(request.POST, request.FILES)
         if form.is_valid():
             instance = form.save(commit=False)
             instance.authuser = request.user
