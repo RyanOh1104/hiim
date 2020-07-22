d72036b9b1b611f009552e210a015b56e4558ef5
Ryan <yyjo1104@naver.com>
2020-07-22 16:51:54 +0900

    hiim page

--- qanda/models.py
+++ qanda/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
-class Question(model.Model):
+class Question(models.Model):
     objects = models.Manager()
     
     number = models.IntegerField(primary_key=True)
