b3c052f754942f5b1dc645a54c06d3af899a0baa
Ryan <yyjo1104@naver.com>
2020-07-23 21:03:05 +0900

    count clicks

--- qanda/views.py
+++ qanda/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.forms import modelformset_factory, inlineformset_factory
-from .models import Question, Answer
+# from .models import Question, Answer
 from django.utils import timezone
 from datetime import datetime, date
 from django_slugify_processor.text import slugify
