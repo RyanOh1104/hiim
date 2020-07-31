2fd13efa90777e32b8bfe1a9a86f618341f674f1
Ryan <yyjo1104@naver.com>
2020-07-31 16:00:46 +0900

    minor change

--- qanda/models.py
+++ qanda/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class QuestionCategory(models.Model):
-    category = models.CharField(max_length=20, default="", primary_key = True, db_column='category_id')
+    category = models.CharField(max_length=20, default="", primary_key = True)
     categoryEng = models.CharField(max_length=50, default="")
 
     def __str__(self):
@@ -12,10 +12,10 @@ class Question(models.Model):
     number = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=20, default="")
     question = models.CharField(max_length=1000, default="")
-    category = models.ForeignKey(QuestionCategory, to_field='category', on_delete=models.CASCADE, null=True, default=None)
+    category_id = models.ForeignKey(QuestionCategory, to_field='category', on_delete=models.CASCADE, null=True, default=None)
 
     def __str__(self):
-        return (str(self.category)+" "+str(self.number)+". "+str(self.title))
+        return (str(self.category_id)+" "+str(self.number)+". "+str(self.title))
 
 class Answer(models.Model):
     authuser = models.ForeignKey(User, on_delete=models.CASCADE, null=True, default=None)
