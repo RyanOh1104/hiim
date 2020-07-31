c3d814780d2219a951402ae710a19a4f4fb9e9a6
Ryan <yyjo1104@naver.com>
2020-07-31 17:38:47 +0900

    qanda db

--- qanda/models.py
+++ qanda/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+'''
 class QuestionCategory(models.Model):
     category = models.CharField(max_length=20, default="", primary_key = True)
     categoryEng = models.CharField(max_length=50, default="")
@@ -30,3 +31,4 @@ class Answer(models.Model):
     def __str__(self):
         return (str(self.questionNumber)+"--answered by--"+str(self.authuser)+"--on "+str(self.dateAnswered)[:11])
 
+'''
