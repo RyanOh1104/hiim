5e3a7d8f457963fa3362b9771025bf79e246e8d4
Ryan <yyjo1104@naver.com>
2020-07-31 15:51:08 +0900

    minor change

--- qanda/models.py
+++ qanda/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class QuestionCategory(models.Model):
-    category = models.CharField(max_length=20, default="", unique=True)
+    category = models.CharField(max_length=20, default="", primary_key = True)
     categoryEng = models.CharField(max_length=50, default="")
 
     def __str__(self):
