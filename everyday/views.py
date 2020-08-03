275aebf4f6f0e48065b5b2ac40d03576495bfce0
Ryan <yyjo1104@naver.com>
2020-08-03 10:20:39 +0900

    minor change

--- everyday/views.py
+++ everyday/views.py
@@ -36,8 +36,9 @@ def everydayinput(request):
                 instance.kw2 = "&nbsp;"
             if instance.kw3 == "":
                 instance.kw3 = "&nbsp;"
-            if instance.emoji = "":
-                instance.emoji = "✔"
+
+            # if instance.emoji = "":
+            #     instance.emoji = "✔"
 
             instance.slug = slugify(today)
             instance.url = "/everydaydetail/" + str(instance.authuser_id) + '/' + str(instance.slug)
