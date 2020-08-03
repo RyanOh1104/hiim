a4ef7bf932da895bd10e6eb036c67b635105b98d
Ryan <yyjo1104@naver.com>
2020-08-03 12:09:34 +0900

    minor change

--- everyday/static/everyday/everyday.js
+++ everyday/static/everyday/everyday.js
@@ -143,6 +143,7 @@ function buildCalendar(){//현재 달 달력 만들기
      }
 }
 function randomPlaceholder() {
+  console.log('function called');
   var questions = [
     '기억 낚아올리기 : 오늘 먹었던 음식 중 뭐가 제일 기억에 남아요?',
     '기억 낚아올리기 : 오늘 하루 아쉬웠던 점이 있다면?',
@@ -151,7 +152,7 @@ function randomPlaceholder() {
     '기억 낚아올리기 : ccc',
   ];
   const randomQuestion = questions[Math.floor(Math.random() * questions.length)];
-  
+  console.log('random is: ', randomQuestion);
   document.getElementById('what')[0].setAttribute('placeholder', randomQuestion);
 }
 
