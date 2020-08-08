3f26581f0aa75d4a6458cf0334af215b6eb47717
Ryan <yyjo1104@naver.com>
2020-08-08 17:37:27 +0900

    css major change

--- everyday/static/everyday/everyday_placeholder.js
+++ everyday/static/everyday/everyday_placeholder.js
@@ -7,6 +7,7 @@ function randomPlaceholder() {
       '[기억 낚아올리기] 오늘이 어제와 달랐던 점은 뭔가요?',
       '[기억 낚아올리기] 오늘 시간이 가장 빨리 간 때는 언제인가요?',
       '[기억 낚아올리기] 오늘을 하나의 단어로 표현한다면?',
+      '[기억 낚아올리기] 오늘이 어제와 달랐던 점 한가지를 꼽으라면?'
     ];
     const randomQuestion = questions[Math.floor(Math.random() * questions.length)];
     console.log('random is: ', randomQuestion);
