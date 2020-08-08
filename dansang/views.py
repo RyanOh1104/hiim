bacd05f40b31ba8b516ae6f77fe052c2ca562b56
Ryan <yyjo1104@naver.com>
2020-08-08 21:27:12 +0900

    dansang category

--- dansang/views.py
+++ dansang/views.py
@@ -56,7 +56,7 @@ def dansangmain(request):
     dansangs = DansangInput.objects.filter(authuser=request.user).order_by('-created')
     how_many = dansangs.count()
     
-    categories = list(DansangInput.objects.filter(authuser=request.user).values('category'))
+    categories = DansangInput.objects.filter(authuser=request.user).values('category')
 
     # pagination
     dansangPaginator = Paginator(dansangs, 7)
