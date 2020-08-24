bff5f07ec8d4dda40edc49042b2a7eb06cb66244
Ryan <yyjo1104@naver.com>
2020-08-24 16:14:06 +0900

    refactoring

--- qanda/urls.py
+++ qanda/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('qandacreate', views.qandaCreate, name='qandacreate'),
+    path('question', views.qandaCreate, name='question'),
     path('qandamain', views.qandaMain, name='qandamain'),
     path('answer/<int:questionNumber>', views.qandaDetail, name='qandadetail'),
     path('update/<int:questionNumber>', views.qandaUpdate, name='qandaupdate'),
