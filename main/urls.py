f814112c1166fcc49ad0b616a0ab53687c6d5a5d
Ryan <yyjo1104@naver.com>
2020-08-28 18:07:23 +0900

    backend commenting

--- main/urls.py
+++ main/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from main import views
 
 urlpatterns = [
-    path('inputuserinfo', views.inputuserinfo, name='inputuserinfo'),
+    path('input-user-info', views.inputUserinfo, name='inputUserinfo'),
     path('', views.usermain, name='usermain'),
     path('welcome', views.landing, name="landing"),
     path('hiim', views.hiim, name="hiim"),
     path('tong', views.tong, name='tong'),
-    path('update/<int:authuser_id>', views.update, name='update'),
+    path('update/<int:authuser_id>', views.updateUserinfo, name='update'),
 ]
